19a3e81b8b1965c1c7727ec2afc20b5d4a6e1da7
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-01-21 16:04:01 +0300

    feat:add polyfill for isNaN()

--- polyfill.js
+++ polyfill.js
@@ -1,7 +1,12 @@
-// ! Method Number.isNaN()
+// ! Method Number.isNaN() and isNan()
 // TODO: The following works because NaN is the only value in JavaScript which is not equal to itself.
 {
 	Number.isNaN = Number.isNaN || function isNaN(input) {
 		return typeof input === 'number' && input !== input;
 	}
+
+	function isNaN(input) {
+    const n = Number(value);
+    return n !== n;
+  }
 }
