37f21aeffb95b003f2cc1bbca9e783d0754f3ba3
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-12-02 14:04:49 +0300

    feat: add quicksort and binary search

--- algoritms.js
+++ algoritms.js
@@ -1,6 +1,11 @@
 //! Sort
 let countBubbleSort = 0;
 let countSelectionSort = 0;
+let countQuickSort = 0;
+let countBinary = 0;
+let countBinaryRecursive = 0;
+
+const arrSorted = [-3, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 const array = Array(100)
   .fill(0)
@@ -42,22 +47,98 @@ const getCompare = (a, b) => {
 
 //TODO: Selection Sort
 {
-	const selectSort = (input, compare = getCompare) => {
-		const { length } = input;
+  const selectSort = (input, compare = getCompare) => {
+    const { length } = input;
     for (let i = 0; i < length - 1; i++) {
-			let indexMin = i;
-			for (let j = i + 1; j < length; j++) {
-				countSelectionSort++;
-				if (compare(input[indexMin], input[j]) === Compare.BIGGER_THAN) {
-					indexMin = j;
-				}
-			}
-			if (indexMin !== i) {
-				swap(input, i, indexMin);
-			}
+      let indexMin = i;
+      for (let j = i + 1; j < length; j++) {
+        countSelectionSort++;
+        if (compare(input[indexMin], input[j]) === Compare.BIGGER_THAN) {
+          indexMin = j;
+        }
+      }
+      if (indexMin !== i) {
+        swap(input, i, indexMin);
+      }
     }
-		return input;
-	};
-	console.log(selectSort([...array]));
+    return input;
+  };
+  console.log(selectSort([...array]));
   console.log(countSelectionSort);
+}
+
+//TODO: Quick Sort with recursive function
+{
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const quickSort = (arr, compare = getCompare) => {
+    const { length } = arr;
+    if (length <= 1) {
+      return arr;
+    }
+
+    const lessArr = [];
+    const biggerArr = [];
+    const pivot = arr[0];
+
+    for (let i = 1; i < length; i++) {
+      countQuickSort++;
+      if (compare(arr[i], pivot) === Compare.LESS_THAN) {
+        lessArr.push(arr[i]);
+      } else {
+        biggerArr.push(arr[i]);
+      }
+    }
+    return quickSort(lessArr).concat(pivot, quickSort(biggerArr));
+  };
+  console.log(quickSort([...array]));
+  console.log(countQuickSort);
+}
+
+//! Binary search
+//TODO: Binary search
+{
+  const search = (arr, target) => {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle;
+
+    while (start <= end) {
+      countBinary++;
+      middle = Math.floor((start + end) / 2);
+
+      if (target === arr[middle]) {
+        return middle;
+      } else if (target < arr[middle]) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+
+    return -1;
+  };
+
+  console.log(search(arrSorted, 14));
+  console.log(countBinary);
+}
+//TODO: Binary search with recursive function
+{
+  const searchRecursive = (arr, target, start = 0, end = arr.length - 1) => {
+    countBinaryRecursive++;
+    let middle = Math.floor((start + end) / 2);
+
+    if (target === arr[middle]) {
+      return middle;
+    } else if (target < arr[middle]) {
+      return searchRecursive(arr, target, start, middle - 1);
+    } else if (target > arr[middle]) {
+      return searchRecursive(arr, target, middle + 1, end);
+    } else {
+      return -1;
+    }
+  };
+
+  console.log(searchRecursive(arrSorted, 14));
+  console.log(countBinaryRecursive);
 }
