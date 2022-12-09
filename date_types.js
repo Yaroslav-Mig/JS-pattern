1561d717a7a4ac594813ad442a59d3fdb119f9b1
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-12-09 18:29:03 +0300

    feat: add compare and type of var functions

--- date_types.js
+++ date_types.js
@@ -1,4 +1,4 @@
-// !
+// ! Transform primitive objects to primitive
 // TODO: way to check if it a variable a number
 {
   function checkIsNumber(value) {
@@ -8,10 +8,23 @@
     return !isNaN(value) && Number.isFinite(value);
   }
 }
+// TODO: use valueOf() and toString()
+{
+  const getPrimitive = (input) => {
+    if (input instanceof Number) {
+      return input.valueOf();
+    } else if (input instanceof String) {
+      return input.toString();
+    } else if (input instanceof Boolean) {
+      return input.valueOf();
+    }
+  };
+}
+
 //! Get sum of all digit the number
 // TODO: Use toString().split() and reduce()
 {
-	function getSum(number) {
+  function getSum(number) {
     return number
       .toString()
       .split('')
@@ -26,6 +39,23 @@
 }
 // TODO: Sum of numbers from 0 to N
 {
-	const sumFirstNumbers = (N) => Boolean(N) ? N + sumFirstNumbers(N - 1) : N;
+  const sumFirstNumbers = (N) => (Boolean(N) ? N + sumFirstNumbers(N - 1) : N);
 }
 
+//! Get Typeof
+{
+  const isObject = (obj) => {
+    return obj !== null && typeof obj === 'object';
+  };
+
+  const getTypeOf = (input) => {
+    return Object.prototype.toString.call(input).slice(8, -1).toLowerCase();
+  };
+  console.log(getTypeOf('1'));
+  console.log(getTypeOf(1));
+  console.log(getTypeOf(null));
+  console.log(getTypeOf(undefined));
+  console.log(getTypeOf([]));
+  console.log(getTypeOf({}));
+  console.log(getTypeOf(() => {}));
+}
--- object.js
+++ object.js
@@ -30,33 +30,33 @@
   let obj3 = JSON.parse(JSON.stringify(obj1));
   obj1.a = 4;
   obj1.b.c = 4;
-	console.log(JSON.stringify(obj3)); // { "a": 0, "b": { "c": 0}}
+  console.log(JSON.stringify(obj3)); // { "a": 0, "b": { "c": 0}}
 
- // TODO: deep clone objects or array
-	const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+  // TODO: deep clone objects or array
+  const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
 
-	const deepClone = (input) => {
-		if (input === null || typeof input !== 'object') {
-			return input;
-		}
+  const deepClone = (input) => {
+    if (input === null || typeof input !== 'object') {
+      return input;
+    }
 
-		const initialInput = Array.isArray(input) ? [] : {};
+    const initialInput = Array.isArray(input) ? [] : {};
 
-		return Object.keys(input).reduce((acc, key) => {
-			acc[key] = deepClone(input[key]);
-			return acc;
-		}, initialInput)
-	}
+    return Object.keys(input).reduce((acc, key) => {
+      acc[key] = deepClone(input[key]);
+      return acc;
+    }, initialInput);
+  };
 
-	const newObj = deepClone(obj1);
-	console.log(newObj === obj1);
-	obj1.b.d = 1;
-	console.log(newObj, obj1);
-	console.log('---------');
+  const newObj = deepClone(obj1);
+  console.log(newObj === obj1);
+  obj1.b.d = 1;
+  console.log(newObj, obj1);
+  console.log('---------');
 
-	const newArr = deepClone(arr4);
-	console.log(newArr === arr4);
-	console.log(newArr, arr4);
+  const newArr = deepClone(arr4);
+  console.log(newArr === arr4);
+  console.log(newArr, arr4);
 }
 // ! Delete field in a obj with Destructuring assignment and rest
 {
@@ -71,4 +71,181 @@
     return other;
   }
   console.log(deleteField('name', person));
+}
+
+//! Compare Objects
+const isObject = (obj) => {
+  return obj !== null && typeof obj === 'object';
+};
+
+const getTypeOf = (input) => {
+  return Object.prototype.toString.call(input).slice(8, -1).toLowerCase();
+};
+
+const getPrimitive = (input) => {
+  if (input instanceof Number) {
+    return input.valueOf();
+  } else if (input instanceof String) {
+    return input.toString();
+  } else if (input instanceof Boolean) {
+    return input.valueOf();
+  }
+};
+
+// TODO: Shallow equality
+{
+  const hero1 = {
+    name: 'Batman',
+    realName: 'Bruce Wayne',
+  };
+  const hero2 = {
+    name: 'Batman',
+    realName: 'Bruce Wayne',
+  };
+  const hero3 = {
+    name: 'Joker',
+  };
+
+  const shallowEqualObj = (obj_1, obj_2) => {
+    const keys_1 = Object.keys(obj_1);
+    const keys_2 = Object.keys(obj_2);
+
+    if (keys_1.length !== keys_2.length) return false;
+
+    for (const key of keys_1) {
+      if (obj_1[key] !== obj_2[key]) return false;
+    }
+    return true;
+  };
+  shallowEqualObj(hero1, hero2);
+  shallowEqualObj(hero1, hero3);
+
+  const shallowCompare = (obj_1, obj_2) => {
+    const primitive = ['string', 'number', 'boolean'];
+    const typeObj_1 = getTypeOf(obj_1);
+    const typeObj_2 = getTypeOf(obj_2);
+
+    if (primitive.includes(typeObj_1) && typeof obj_1 === 'object') {
+      obj_1 = getPrimitive(obj_1);
+    }
+    if (primitive.includes(typeObj_2) && typeof obj_2 === 'object') {
+      obj_2 = getPrimitive(obj_2);
+    }
+
+    if (typeObj_1 !== typeObj_2) {
+      return false;
+    }
+
+    if (typeObj_1 === 'array') {
+      if (obj_1.length !== obj_2.length) {
+        return false;
+      }
+      return obj_1.every((el, ind) => el === obj_2[ind]);
+    }
+    if (typeObj_1 === 'object') {
+      const keys_1 = Object.keys(obj_1);
+      const keys_2 = Object.keys(obj_2);
+
+      if (keys_1.length !== keys_2.length) {
+        return false;
+      }
+      return keys_1.every((key) => obj_1[key] === obj_2[key]);
+    }
+    if (typeObj_1 === 'date') {
+      return obj_1.getTime() === obj_2.getTime();
+    }
+
+    return obj_1 === obj_2;
+  };
+}
+// TODO: Deep equality
+{
+  const hero1 = {
+    name: 'Batman',
+    address: {
+      city: 'Gotham',
+    },
+		skills: [1, 2, 3],
+		superPower: true
+  };
+  const hero2 = {
+    name: 'Batman',
+    address: {
+      city: 'Gotham',
+    },
+		skills: [1, 2, 3],
+		superPower: true
+  };
+  const deepEqualObj = (obj_1, obj_2) => {
+    const keys_1 = Object.keys(obj_1);
+    const keys_2 = Object.keys(obj_2);
+
+    if (keys_1.length !== keys_2.length) return false;
+
+    for (const key of keys_1) {
+      const val_1 = obj_1[key];
+      const val_2 = obj_2[key];
+      const areObjects = isObject(val_1) && isObject(val_2);
+      if ((areObjects && !deepEqualObj(val_1, val_2)) || (!areObjects && val_1 !== val_2)) {
+        return false;
+      }
+    }
+    return true;
+  };
+  // console.log(deepEqualObj({ name1: 'abc' }, { name2: 'abc' }));
+  // console.log(deepEqualObj(hero1, hero2));
+  // console.log(deepEqualObj([1, 2], [1, 2]));
+  // console.log(deepEqualObj(1, 2));
+  const deepCompare = (obj_1, obj_2) => {
+    const primitive = ['string', 'number', 'boolean'];
+    const typeObj_1 = getTypeOf(obj_1);
+    const typeObj_2 = getTypeOf(obj_2);
+
+    if (primitive.includes(typeObj_1) && typeof obj_1 === 'object') {
+      obj_1 = getPrimitive(obj_1);
+    }
+    if (primitive.includes(typeObj_2) && typeof obj_2 === 'object') {
+      obj_2 = getPrimitive(obj_2);
+    }
+
+    if (typeObj_1 !== typeObj_2) {
+      return false;
+    }
+
+    if (typeObj_1 === 'array') {
+      if (obj_1.length !== obj_2.length) {
+        return false;
+      }
+      return obj_1.every((el, ind) => deepCompare(el, obj_2[ind]));
+    }
+    if (typeObj_1 === 'object') {
+      const keys_1 = Object.keys(obj_1);
+      const keys_2 = Object.keys(obj_2);
+
+      if (keys_1.length !== keys_2.length) {
+        return false;
+      }
+      return keys_1.every((key) => deepCompare(obj_1[key], obj_2[key]));
+    }
+    if (typeObj_1 === 'date') {
+      return obj_1.getTime() === obj_2.getTime();
+    }
+
+    return obj_1 === obj_2;
+  };
+  console.log(deepCompare({ name1: 'abc' }, { name2: 'abc' }));
+  console.log(deepCompare({ name1: 'abc' }, { name1: 'abc' }));
+  console.log(deepCompare(hero1, hero2));
+  console.log(deepCompare([1, 2], [1, 2]));
+  console.log(deepCompare([1, 2], [1, 2, 3]));
+  console.log(deepCompare(true, false));
+  console.log(deepCompare(1, 1));
+  console.log(deepCompare(null, null));
+  console.log(deepCompare(undefined, undefined));
+  console.log(deepCompare(2, new Number(2)));
+  console.log(deepCompare(true, new Boolean(true)));
+  console.log(deepCompare('abc', new String('abc')));
+  console.log(deepCompare('abc', new String('abcd')));
+	console.log(deepCompare(new Date(), new Date()));
+	console.log(deepCompare(hero1, hero2));
 }
