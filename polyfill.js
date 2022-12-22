be968332d3084768dce2413bfebe9b9fad112544
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-12-22 16:55:54 +0300

    feat: add polyfill for native methods bind, call, apply

--- polyfill.js
+++ polyfill.js
@@ -38,8 +38,8 @@
   const array = [1, 2, 3];
   const doubleFn = (item) => item * 2;
 
-	function mapFn(arr, fn) {
-		const mappedArr = [];
+  function mapFn(arr, fn) {
+    const mappedArr = [];
 
     for (let i = 0; i < arr.length; i++) {
       mappedArr[i] = fn(arr[i]);
@@ -74,4 +74,75 @@
     }
     return newArr;
   };
+}
+//! method bind(), call()
+{
+  const person = {
+    userName: 'Yar',
+  };
+  function showInfo(cell, email) {
+    console.log(`Name: ${this.userName}, cell: ${cell}, e-mail: ${email}`);
+  }
+  //TODO: ES6 + new method in the object
+  {
+    function bind(fn, context, ...rest) {
+      return function (...args) {
+        const uniqID = Date.now().toString();
+        context[uniqID] = fn;
+        const result = context[uniqID](...rest.concat(args));
+        delete context[uniqID];
+        return result;
+      };
+    }
+
+    bind(showInfo, person, '312-155', 'yar@mail.ru')();
+    bind(showInfo, person, '312-155')('yar@mail.ru');
+    bind(showInfo, person)('312-155', 'yar@mail.ru');
+    console.log('---------------');
+  }
+  //TODO: ES5
+  {
+    function bind(fn, context) {
+      let rest = Array.prototype.slice.call(arguments, 2);
+
+      return function () {
+        const args = Array.from(arguments);
+        return fn.apply(context, rest.concat(args));
+      };
+    }
+    bind(showInfo, person, '312-155', 'yar@mail.ru')();
+    bind(showInfo, person, '312-155')('yar@mail.ru');
+    bind(showInfo, person)('312-155', 'yar@mail.ru');
+  }
+  //TODO: ES6
+  {
+    function bind(fn, context, ...rest) {
+      return function (...args) {
+        return fn.apply(context, rest.concat(args));
+      };
+    }
+	}
+//TODO: call()
+	{
+		function call(fn, context, ...args) {
+			const uniqID = Date.now().toString();
+			context[uniqID] = fn;
+			const result = context[uniqID](...args);
+			delete context[uniqID];
+			return result;
+		}
+	}
+	call(showInfo, person, '458-465', 'bik@mail.ru')
+
+	//TODO: apply()
+	{
+		function apply(fn, context, args) {
+			const uniqID = Date.now().toString();
+			context[uniqID] = fn;
+			const result = context[uniqID](...args);
+			delete context[uniqID];
+			return result;
+		}
+	}
+	apply(showInfo, person, ['458-465', 'bik@mail.ru'])
 }
